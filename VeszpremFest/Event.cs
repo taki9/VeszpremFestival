756d39683929305f4623ce54082344e631d5a42b
Takács Zoltán <zoltandavidtakacs@gmail.com>
2016-05-02 19:52:44 +0200

    Elbasztad Ákos

---
+++ VeszpremFest/Event.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace server
+{
+    class Event
+    {
+        private int _id;
+        private string _performName;
+        Location location;
+        private DateTime _start;
+        private int _availSeats;
+
+        public Event(int id, string performName, Location location, DateTime start, int availSeats)
+        {
+            Id = id;
+            PerformName = performName;
+            Location = location;
+            Start = start;
+            AvailSeats = availSeats;
+        }
+
+        public DateTime Start
+        {
+            get
+            {
+                return _start;
+            }
+
+            set
+            {
+                _start = value;
+            }
+        }
+
+        public int AvailSeats
+        {
+            get
+            {
+                return _availSeats;
+            }
+
+            set
+            {
+                _availSeats = value;
+            }
+        }
+
+        public string PerformName
+        {
+            get
+            {
+                return _performName;
+            }
+
+            set
+            {
+                _performName = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+
+            set
+            {
+                _id = value;
+            }
+        }
+
+        internal Location Location
+        {
+            get
+            {
+                return location;
+            }
+
+            set
+            {
+                location = value;
+            }
+        }
+
+
+    }
+}
---
+++ VeszpremFest/OrderController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server
+{
+    class OrderController
+    {
+        private List<Order> _orders;
+
+        public OrderController()
+        {
+            Orders = new List<Order>();
+        }
+
+        internal List<Order> Orders
+        {
+            get
+            {
+                return _orders;
+            }
+
+            set
+            {
+                _orders = value;
+            }
+        }
+
+        public void readOrdersFromDb(int userID, EventController evc, DbController dbc)
+        {
+            DataTable orderTable = dbc.readOrders(userID);
+
+            foreach (DataRow row in orderTable.Rows)
+            {
+                Event ev = evc.getEventByID(Convert.ToInt32(row.Field<Int64>("Event_ID")));
+
+                Order order = new Order(ev.PerformName, ev.Location.Name, ev.Start);
+
+                DataTable seatTable = dbc.readSeatsForEvent(ev.Id, userID);
+
+                foreach (DataRow seat in seatTable.Rows)
+                {
+                    order.addSeat(new Seat(Convert.ToInt32(seat.Field<Int64>("RowNumber")), Convert.ToInt32(seat.Field<Int64>("ColumnNumber"))));
+                }
+
+                Orders.Add(order);
+            }
+        }
+
+        public int numOfOrders()
+        {
+            return Orders.Count();
+        }
+
+        public Order getOrder(int index)
+        {
+            return Orders[index];
+        }
+
+        public bool newOrder(Order order)
+        {
+            int i = 0;
+
+            while (i < Orders.Count && Orders[i].Perform != order.Perform && Orders[i].Loc != order.Loc && !(Orders[i].Start.Equals(order.Start)))
+            {
+                i++;
+            }
+
+            if (i < Orders.Count)
+            {
+                Orders[i].addSeat(order.Seats[0]);
+
+                return true;
+            }
+
+            Orders.Add(order);
+
+            return false;
+        }
+
+    }
+}
